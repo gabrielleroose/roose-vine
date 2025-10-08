54caed3324d83165fa802b72f163f89dc5144868
groose <groose@iu.edu>
2025-10-07 21:23:18 -0400

    Testing php in burrow

--- public.php
+++ public.php
@@ -1,3 +1,7 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,16 +11,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <style>
-        <?php include 'css/styles.css' ?> 
-        <?php include 'css/grid.css' ?> 
-
+        <?php include 'css/styles.css'; ?> 
+        <?php include 'css/grid.css'; ?> 
     </style>  
-    
 </head>
 <body>
+
+  
 <?php include 'includes/navbar.php'; ?>
+
 <?php
-// Connect to database
 $servername = "db.luddy.indiana.edu";
 $username = "i494f25_groose"; 
 $password = "prose3351skite";
@@ -24,18 +28,23 @@ $dbname = "i494f25_groose";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    echo '<div class="alert alert-danger text-center" role="alert">
+            ❌ Connection failed: ' . $conn->connect_error . '
+          </div>';
+} else {
+    echo '<div class="alert alert-success text-center" role="alert">
+            ✅ Connection successful!
+          </div>';
 }
 ?>
 
 <table class="dish-table">
      <tr>
-     <th>Dish Name</th>
-     <th>Dish Price</th>
-     <th>Dish Category</th>
-     <th>Dish Description</th>
+         <th>Dish Name</th>
+         <th>Dish Price</th>
+         <th>Dish Category</th>
+         <th>Dish Description</th>
      </tr>
      
      <tr>
@@ -53,10 +62,10 @@ if ($conn->connect_error) {
      </tr>
 
      <tr>
-        <td>Chicken Sandwhich</td>
+        <td>Chicken Sandwich</td>
         <td>$11.99</td>
         <td>American</td>
-        <td>Fried Chicken Sandwhich w/ Cheese</td>
+        <td>Fried Chicken Sandwich w/ Cheese</td>
      </tr>
 
      <tr>
@@ -65,17 +74,15 @@ if ($conn->connect_error) {
         <td>Italian</td>
         <td>Pasta Salad with tomatoes & salami</td>
      </tr>
+</table>
 
-  </table>
-
-  <h2>Food Gallery</h2>
-  <div class="food-gallery">
+<h2>Food Gallery</h2>
+<div class="food-gallery">
     <img src="img/enchiladas.jpg" alt="image of enchiladas on a plate">
-    <img src="img/cheeseburger.jpg" alt="image of a cheesrburger and fries on a plate">
-    <img src="img/chicken-sandwhich.jpg" alt="close up image of a chicken sandwhich">
+    <img src="img/cheeseburger.jpg" alt="image of a cheeseburger and fries on a plate">
+    <img src="img/chicken-sandwhich.jpg" alt="close up image of a chicken sandwich">
     <img src="img/pasta-salad.jpg" alt="bowl of pasta salad">
+</div>
 
-  </div>
-    
 </body>
 </html>
